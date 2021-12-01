0942f7bb59daf3a0c80ff70986e5af1f1db8d968
alcazarlo <alcazarlo@esat-miralles.com>
2021-12-01 13:41:32 +0100

    Updated matrix constructors

--- include/lib_math/matrix4.h
+++ include/lib_math/matrix4.h
@@ -62,7 +62,7 @@ class Matix4x4{
   Matix4x4 PerspectiveMatrix(float fov, float aspect,
 	  float near, float far) const;
 
-  Matix4x4 OrthoMatrix(float right, float left, float top, float valueottom,
+  Matix4x4 OrthoMatrix(float right, float left, float top, float bottom,
 	  float near, float far) const;
 
   Vector4 GetColum(int colum) const;
@@ -73,23 +73,31 @@ class Matix4x4{
 };
 
 inline Matix4x4::Matix4x4() {
-  for(int i = 0; i < 16; ++i)
-    m[i];
+  m[0] = 0.0f; m[4] = 0.0f; m[8] = 0.0f; m[12] = 0.0f;
+  m[1] = 0.0f; m[5] = 0.0f; m[9] = 0.0f; m[13] = 0.0f;
+  m[2] = 0.0f; m[6] = 0.0f; m[10] = 0.0f; m[14] = 0.0f;
+  m[3] = 0.0f; m[7] = 0.0f; m[11] = 0.0f; m[15] = 0.0f;
 }
 
 inline Matix4x4::Matix4x4(float array[16]) {
-  for(int i = 0; i < 16; ++i)
-    m[i] = array[i];
+  m[0] = array[0]; m[4] = array[4]; m[8] = array[8]; m[12] = array[12];
+  m[1] = array[1]; m[5] = array[5]; m[9] = array[9]; m[13] = array[13];
+  m[2] = array[2]; m[6] = array[6]; m[10] = array[10]; m[14] = array[14];
+  m[3] = array[3]; m[7] = array[7]; m[11] = array[11]; m[15] = array[15];
 }
 
 inline Matix4x4::Matix4x4(float value) {
-  for(int i = 0; i < 16; ++i)
-    m[i] = value;
+  m[0] = value; m[4] = value; m[8] = value; m[12] = value;
+  m[1] = value; m[5] = value; m[9] = value; m[13] = value;
+  m[2] = value; m[6] = value; m[10] = value; m[14] = value;
+  m[3] = value; m[7] = value; m[11] = value; m[15] = value;
 }
 
 inline Matix4x4::Matix4x4(const Matix4x4& copy) {
-  for(int i = 0; i < 16; ++i)
-    m[i] = copy.m[i];
+  m[0] = copy.m[0]; m[4] = copy.m[4]; m[8] = copy.m[8]; m[12] = copy.m[12];
+  m[1] = copy.m[1]; m[5] = copy.m[5]; m[9] = copy.m[9]; m[13] = copy.m[13];
+  m[2] = copy.m[2]; m[6] = copy.m[6]; m[10] = copy.m[10]; m[14] = copy.m[14];
+  m[3] = copy.m[3]; m[7] = copy.m[7]; m[11] = copy.m[11]; m[15] = copy.m[15];
 }
 
 inline Matix4x4::~Matix4x4() {}
@@ -151,8 +159,11 @@ inline bool Matix4x4::GetInverse(Matix4x4* out) const {
 }
 
 inline Matix4x4 Matix4x4::Transpose() const {
-    
-  return Matix4x4();
+  float array[16] = { this->m[0], this->m[1], this->m[2], this->m[3],
+                      this->m[4], this->m[5], this->m[6], this->m[7],
+                      this->m[8], this->m[9], this->m[10], this->m[11],
+                      this->m[12], this->m[13], this->m[14], this->m[15] };
+  return Matix4x4(array);
 }
 
 inline Matix4x4 Matix4x4::Translate(const Vector3& distance){
@@ -175,7 +186,7 @@ inline Matix4x4 Matix4x4::Scale(const Vector3& scale){
   float array[16] = { scale.x, 0.0f, 0.0f, 0.0f,
                     0.0f, scale.y, 0.0f, 0.0f,
                     0.0f, 0.0f, scale.z, 0.0f,
-                    0.0f, 0.0f, 0.0f ,0.0f    };
+                    0.0f, 0.0f, 0.0f, 0.0f    };
   return Matix4x4(array);
 }
 
@@ -183,7 +194,7 @@ inline Matix4x4 Matix4x4::Scale(float x, float y, float z){
  float array[16] = { x, 0.0f, 0.0f, 0.0f,
                     0.0f, y, 0.0f, 0.0f,
                     0.0f, 0.0f, z, 0.0f,
-                    0.0f, 0.0f, 0.0f ,0.0f };
+                    0.0f, 0.0f, 0.0f, 0.0f };
   return Matix4x4(array);
 }
 
@@ -248,7 +259,7 @@ inline Matix4x4 Matix4x4::PerspectiveMatrix(float fov, float aspect,
   return Matix4x4();
 }
 
-inline Matix4x4 Matix4x4::OrthoMatrix(float right, float left, float top, float valueottom,
+inline Matix4x4 Matix4x4::OrthoMatrix(float right, float left, float top, float bottom,
   float near, float far) const {
 
   return Matix4x4();
